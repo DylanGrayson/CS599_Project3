5724c2dd7f1c8fb9b7c3c62962fe1ba7011442f8
Dylan Grayson <dgrayson11@gmail.com>
2016-04-15 14:05:39 -0700

    ezpz

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*****
+ * Dylan Grayson
+ * Conner Swann
+ * Brandon Paree
+ * 
+ * CS599 Project 3
+ * 
+ * Phylogenetic / Metagenomic Binning
+******/
+
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
