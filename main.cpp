c46b4f94161abeff5dd5949c9c3373fbbe837379
Dylan Grayson <dgrayson11@gmail.com>
2016-04-26 16:05:50 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@
 
 using namespace std;
 
+
+//Sequence Class contains a long int sequenceLoc that gets passed to 
+//file.seekg() to get the actual sequence.
 class Sequence {
 	public:
 		string sequenceId;
@@ -36,13 +39,14 @@ class Sequence {
 		long int sequenceLoc;
 };
 
+//Read class
 class Read {
 	public:
 		string readId;
 		string sequence;
 };
 		
-
+//Bucket class contains a sequence list and a reads list
 class Bucket {
 	public:
 		string speciesId;
@@ -69,12 +73,14 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 		
 
 int main(int argc, char* argv[]) {
-	//All this should be deleted, just messing around with file I/O
+	//Check that we have two inputs
 	if (argc == 3) {
+		//build our map of buckets from the sequences file
 		map<string, Bucket*> b = getBucketList(argv[1]);
+		//distribute our reads into the buckets
 		distributeReads(b, argv[1], argv[2]);
-	}else {
-		cout << "Usage: " << argv[0] << " <sequence file> <reads file>" << endl;
+	}else { //otherwise print help string
+		cout << "USAGE: " << argv[0] << " <sequence file> <reads file>" << endl;
 	}
 	return 0;
 }
@@ -84,8 +90,9 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 	seqs.open(seqFile);
 	ifstream reads;
 	reads.open(readFile);
-	int poopoopoo = 0;
+	// for each bucket in our bucket list
 	for (map<string, Bucket*>::iterator bucket = bList.begin(); bucket != bList.end(); ++bucket) {
+		//get the list of sequences from this bucket
 		list<Sequence*> seqList = bucket->second->getSeqList();
 		string sequence;
 		for (list<Sequence*>::iterator seq = seqList.begin(); seq != seqList.end(); ++seq) {
@@ -106,6 +113,7 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 	reads.close();
 }
 
+//build map of speciesId to Bucket pointer from a fasta file
 map<string, Bucket*> getBucketList(char * filename) {
 	ifstream seqs;
 	seqs.open(filename);
@@ -117,36 +125,46 @@ map<string, Bucket*> getBucketList(char * filename) {
 	string speciesId;
 	string desc;
 	int count;
+	//Loop through the lines in the file, storing in line string.
 	while ( getline(seqs, line) ){
 		int i = 1;
-		int dashCount = 0;
-		string nameComponent;
-		if (line[0] == '>') {
+		int dashCount = 0; //count dashes seen to know which part of id
+		string nameComponent; //store strings between dashes
+		if (line[0] == '>') { //if this is the label of the sequence
 			while ( line[i] != ' ' && line[i] != '\0'){
-				if (line[i] == '-') {
-					if (dashCount == 0){
+				//while we aren't at the end of the label
+				if (line[i] == '-') { //if we've reached the end of this component
+					if (dashCount == 0){ //sequence id
 						sequenceId = nameComponent;
-					}else if (dashCount == 1) {
+					}else if (dashCount == 1) { //species id
 						speciesId = nameComponent;
-					}else if (dashCount == 2) {
+					}else if (dashCount == 2) { //description
 						desc = nameComponent;
 					}
-					nameComponent = '\0';
+					nameComponent = '\0'; //reset component
 					dashCount++;
 					i++;
 				}
-				nameComponent += line[i];
+				nameComponent += line[i]; //build component
 				i++;
 			}
 			iter = bucketList.find(speciesId);
-			if (iter == bucketList.end()) {
-				Bucket * b = new Bucket(speciesId);
-				bucketList.insert(pair<string, Bucket*>(speciesId, b));
+			if (iter == bucketList.end()) { //if species id doesn't exists in map yet
+				
+				Bucket * b = new Bucket(speciesId); //make new bucket
+				bucketList.insert(pair<string, Bucket*>(speciesId, b)); //and insert it in the map
 			}
-		}else{
+		}else{ //else this is the actual nucleotide sequence
+				//The format of Fasta files guarantee that all variables
+				//Will be set by the previous line loop.
 			long int currLoc = seqs.tellg();
+			//current location is at the end of the sequence, so we set
+			//it to the beginning
 			currLoc -= (line.size()+1)*sizeof(char);
+			
+			//create new sequence
 			Sequence* s = new Sequence(sequenceId, speciesId, currLoc, desc);
+			//and add it to the bucket
 			bucketList.at(speciesId)->insertSequence(s);
 		}
 		count++;
