208bfb812bd279804dcb1030199c9a814f5239c7
Brandon Paree <brandon.paree@gmail.com>
2016-04-30 06:04:39 -0700

    Added some whitespace to make things more readable.

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char* argv[]) {
 	if (argc == 3) {
 		//build our map of buckets from the sequences file
 		map<string, Bucket*> b = getBucketList(argv[1]);
+
 		//distribute our reads into the buckets
 		distributeReads(b, argv[1], argv[2]);
 	}else { //otherwise print help string
@@ -44,6 +45,7 @@ int main(int argc, char* argv[]) {
 void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile) {
 	ifstream seqs;
 	seqs.open(seqFile);
+
 	// for each bucket in our bucket list
 	int counter = 0;
 
@@ -52,15 +54,20 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 	// For each iteration of the thread we can start a new thread?
 
 	for (map<string, Bucket*>::iterator bucket = bList.begin(); bucket != bList.end(); ++bucket) {
+
 		//get the list of sequences from this bucket
 		list<Sequence*> seqList = bucket->second->getSeqList();
 		map<string, long int> *kmerList = new map<string, long int>;
+
 		for (list<Sequence*>::iterator seq = seqList.begin(); seq != seqList.end(); ++seq) {
 			long int location = (*seq)->getLocation();
 			seqs.seekg(location);
 			string line;
+
 			getline(seqs, line);
+
 			long int seqSize = line.size() - K + 1;
+
 			//string *kmers = new string[seqSize];
 			for (long int i = 0; i < seqSize; i++) {
 				kmerList->insert(pair<string, long int>( line.substr(i, K), location+i) );
@@ -73,15 +80,19 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 		string label;
 		long int readLocation;
 		ifstream reads;
+
 		reads.open(readFile);
+
 		while ( getline(reads, read)) { //loop through every line of reads file
 			if (read[0] != '>'){ //if this is the actual nucleotide sequence
+
 				unsigned int sel = 0;
 				string q = read.substr(0, K);
-				while((sel+1)*K < read.size()) { //loop through every K sized chunk of the read
 
+				while((sel+1)*K < read.size()) { //loop through every K sized chunk of the read
 					//try to find the current read in the kmer list
 					map<string, long int>::iterator kmer = kmerList->find(q);
+
 					//if it exists and it extends*
 					if (kmer != kmerList->end() && extends(sel*K, read, kmer->second, &seqs)) {
 						//we create the read object and insert it into the current bucket and break.
@@ -100,7 +111,9 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 		}
 		reads.close();
 		delete kmerList;
+
 		cout << "Bucket " << counter << ") " << bucket->second->getReadCount() << endl;
+
 		counter++;
 	}
 	seqs.close();
@@ -108,15 +121,19 @@ void distributeReads(map<string, Bucket*> bList, char * seqFile, char* readFile)
 
 //extends* function, checks if a read extends w/ M mismatches
 bool extends(int readPos, string read, long int seqPos, ifstream* seqs) {
+
 	//get the "would be" position of the read in the sequence
 	seqs->seekg(seqPos - readPos);
+
 	//get our string read as a classic char * C string
 	const char * cread = read.c_str();
 	int readSize = read.size();
 	char * segment = new char[readSize];
+
 	//get the read-length segment of the corresponding sequence
 	seqs->read(segment, readSize);
 	int mm = 0;
+
 	//for every position in the read/sequence
 	for (int i = 0; i < readSize; i++) {
 		if (i == readPos) {//if this is the K-sized segment from our seed match
@@ -130,6 +147,7 @@ bool extends(int readPos, string read, long int seqPos, ifstream* seqs) {
 			}
 		}
 	}
+
 	//we made it out unscathed, we extend.
 	delete[] segment;
 	return true;
@@ -138,6 +156,7 @@ bool extends(int readPos, string read, long int seqPos, ifstream* seqs) {
 
 //build map of speciesId to Bucket pointer from a fasta file
 map<string, Bucket*> getBucketList(char * filename) {
+
 	ifstream seqs;
 	seqs.open(filename);
 	map<string, Bucket*> bucketList;
@@ -150,9 +169,11 @@ map<string, Bucket*> getBucketList(char * filename) {
 	int count;
 	//Loop through the lines in the file, storing in line string.
 	while ( getline(seqs, line) ){
+
 		int i = 1;
 		int dashCount = 0; //count dashes seen to know which part of id
 		string nameComponent; //store strings between dashes
+
 		if (line[0] == '>') { //if this is the label of the sequence
 			while ( line[i] != ' ' && line[i] != '\0'){
 				//while we aren't at the end of the label
@@ -172,6 +193,7 @@ map<string, Bucket*> getBucketList(char * filename) {
 				i++;
 			}
 			iter = bucketList.find(speciesId);
+
 			if (iter == bucketList.end()) { //if species id doesn't exists in map yet
 
 				Bucket * b = new Bucket(speciesId); //make new bucket
@@ -193,5 +215,6 @@ map<string, Bucket*> getBucketList(char * filename) {
 		count++;
 	}
 	seqs.close();
+
 	return bucketList;
 }
